41a78de707523fbce13cdbc4daf6920b3e17704e
dwayne314 <dwayne.manlove@gmail.com>
2020-05-05 17:34:36 -0400

    Separate views into function by request method

--- app/api/routes.py
+++ app/api/routes.py
@@ -15,15 +15,16 @@ def index():
 # User Routes
 #
 
-@bp.route('/users', methods=['GET', 'POST'])
+@bp.route('/users', methods=['GET'])
 def get_users():
-    """Api router for all users"""
-    if request.method == 'GET':
-        users = Users.serialize_all()
-        message = 'Data found' if users else 'Data not found'
-        return {'success': True, 'message': message, 'data': users}, 200
+    """Gets all users"""
+    users = Users.serialize_all()
+    message = 'Data found' if users else 'Data not found'
+    return {'success': True, 'message': message, 'data': users}, 200
 
-    # Sanitize this request JSON / handle errors
+@bp.route('/users', methods=['POST'])
+def post_users():
+    """Creates a user"""
     new_user = Users(**request.get_json())
     db.session.add(new_user)
     db.session.commit()
@@ -31,41 +32,50 @@ def get_users():
             'message': f'User {new_user.first_name} created',
             'data': {}}, 201
 
-@bp.route('/users/<int:user_id>', methods=['GET', 'PATCH', 'DELETE'])
+@bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    """Api router for a single user resource"""
+    """Gets a user by id"""
     user = Users.serialize_one(user_id)
     if not user:
         return abort(404, {'model': 'User', 'id': user_id})
 
-    if request.method == 'GET':
-        return {'success': True, 'message': 'User found', 'data': user}, 200
+    return {'success': True, 'message': 'User found', 'data': user}, 200
 
-    elif request.method == 'PATCH':
-        # Re-pull the updated attributes to display to the client
-        user = Users.serialize_one(user_id)
+@bp.route('/users/<int:user_id>', methods=['PATCH'])
+def patch_user(user_id):
+    """Patches a user by id"""
+    user = Users.serialize_one(user_id)
+    if not user:
+        return abort(404, {'model': 'User', 'id': user_id})
 
-        message = f'User {user_id} has been updated'
-        return {'success': True, 'message': message, 'data': user}, 200
+    user = Users.serialize_one(user_id)
+    message = f'User {user_id} has been updated'
+    return {'success': True, 'message': message, 'data': user}, 200
 
-    else:
-        message = f'User {user_id} has been deleted'
-        return {'success': True, 'message': message, 'data': {}}, 200
+@bp.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """Deletes a user by id"""
+    user = Users.serialize_one(user_id)
+    if not user:
+        return abort(404, {'model': 'User', 'id': user_id})
 
+    message = f'User {user_id} has been deleted'
+    return {'success': True, 'message': message, 'data': {}}, 200
 
 #
 # Account Routes
 #
 
-@bp.route('/accounts', methods=['GET', 'POST'])
+@bp.route('/accounts', methods=['GET'])
 def get_accounts():
-    """Api router for all accounts"""
-    if request.method == 'GET':
-        accounts = Accounts.serialize_all()
-        message = 'Data found' if accounts else 'Data not found'
-        return {'success': True, 'message': message, 'data': accounts}, 200
+    """Gets all accounts"""
+    accounts = Accounts.serialize_all()
+    message = 'Data found' if accounts else 'Data not found'
+    return {'success': True, 'message': message, 'data': accounts}, 200
 
-    # Sanitize this request JSON / handle errors
+@bp.route('/accounts', methods=['POST'])
+def post_accounts():
+    """Creates an account"""
     new_account = Accounts(**request.get_json())
     db.session.add(new_account)
     db.session.commit()
@@ -75,64 +85,121 @@ def get_accounts():
             'message': f'User {new_account.name} created',
             'data': account}, 201
 
-@bp.route('/accounts/<int:account_id>', methods=['GET', 'PATCH', 'DELETE'])
+@bp.route('/accounts/<int:account_id>', methods=['GET'])
 def get_account(account_id):
-    """Api router for a single account resource"""
+    """Gets an account by id"""
     account = Accounts.serialize_one(account_id)
     if not account:
         return abort(404, {'model': 'Account', 'id': account_id})
 
-    if request.method == 'GET':
-        return {'success': True, 'message': 'Account found', 'data': account}, 200
+    return {'success': True, 'message': 'Account found', 'data': account}, 200
 
-    elif request.method == 'PATCH':
-        # Repull the updated attributes to display to the client
-        account = Users.serialize_one(account_id)
+@bp.route('/accounts/<int:account_id>', methods=['PATCH'])
+def patch_account(account_id):
+    """Patches an account by uid"""
+    account = Accounts.serialize_one(account_id)
+    if not account:
+        return abort(404, {'model': 'Account', 'id': account_id})
 
-        message = f'Account {account_id} has been updated'
-        return {'success': True, 'message': message, 'data': account}, 200
+    account = Users.serialize_one(account_id)
+    message = f'Account {account_id} has been updated'
+    return {'success': True, 'message': message, 'data': account}, 200
 
-    else:
-        message = f'Account {account_id} has been deleted'
-        return {'success': True, 'message': message, 'data': {}}, 200
+
+@bp.route('/accounts/<int:account_id>', methods=['DELETE'])
+def delete_account(account_id):
+    """Deletes a user by id"""
+    account = Accounts.serialize_one(account_id)
+    if not account:
+        return abort(404, {'model': 'Account', 'id': account_id})
+
+    message = f'Account {account_id} has been deleted'
+    return {'success': True, 'message': message, 'data': {}}, 200
 
 #
 # Transaction Routes
 #
 
-@bp.route('/accounts/<int:account_id>/transactions', methods=['GET', 'POST'])
+@bp.route('/accounts/<int:account_id>/transactions', methods=['GET'])
 def get_transactions(account_id):
-    """Api router for all account transactions"""
+    """Gets all transactions for an account"""
     account = Accounts.query.get(account_id)
     transactions = None
 
-    if account:
-        transactions = [Transactions.serialize_one(tran.id) for tran
-                        in account.transactions.all()]
-    else:
+    if not account:
         return abort(404, {'model': 'Account', 'id': account_id})
 
-    if request.method == 'GET':
-        message = 'Data found' if transactions else 'Data not found'
-        return {'success': True,
-                'message': message,
-                'data': transactions}, 200
+    transactions = [Transactions.serialize_one(tran.id) for tran
+                    in account.transactions.all()]
+    message = 'Data found' if transactions else 'Data not found'
+    return {'success': True,
+            'message': message,
+            'data': transactions}, 200
 
-    else:
-        # Sanitize this request JSON / handle errors
-        new_transaction = Transactions(**request.get_json())
-        new_transaction.account_id = account_id
-        db.session.add(new_transaction)
-        db.session.commit()
-        transaction = Transactions.serialize_one(new_transaction.id)
+@bp.route('/accounts/<int:account_id>/transactions', methods=['POST'])
+def post_transactions(account_id):
+    """Creates a transaction for an account"""
+    account = Accounts.query.get(account_id)
 
-        return {'success': True,
-                'message': 'Transaction created',
-                'data': transaction}, 201
+    if not account:
+        return abort(404, {'model': 'Account', 'id': account_id})
 
-@bp.route('/accounts/<int:account_id>/transactions/<int:transaction_id>', 
-          methods=['GET', 'PATCH', 'DELETE'])
+    new_transaction = Transactions(**request.get_json())
+    new_transaction.account_id = account_id
+    db.session.add(new_transaction)
+    db.session.commit()
+    transaction = Transactions.serialize_one(new_transaction.id)
+
+    return {'success': True,
+            'message': 'Transaction created',
+            'data': transaction}, 201
+
+@bp.route('/accounts/<int:account_id>/transactions/<int:transaction_id>',
+          methods=['GET'])
 def get_transaction(account_id, transaction_id):
+    """Gets a transaction from an account by id"""
+    account = Accounts.query.get(account_id)
+    transaction = None
+
+    if account:
+        first_transaction = Transactions.query \
+            .filter_by(account_id=account_id) \
+            .filter_by(id=transaction_id).first()
+
+        if first_transaction:
+            transaction = Transactions.serialize_one(first_transaction.id)
+
+    if not transaction:
+        return abort(404, {'model': 'Transaction', 'id': transaction_id})
+
+    return {'success': True,
+            'message': 'Transaction found',
+            'data': transaction}, 200
+
+@bp.route('/accounts/<int:account_id>/transactions/<int:transaction_id>',
+          methods=['PATCH'])
+def patch_transaction(account_id, transaction_id):
+    """Patches a transaction from an account by id"""
+    account = Accounts.query.get(account_id)
+    transaction = None
+
+    if account:
+        first_transaction = Transactions.query \
+            .filter_by(account_id=account_id) \
+            .filter_by(id=transaction_id).first()
+
+        if first_transaction:
+            transaction = Transactions.serialize_one(first_transaction.id)
+
+    if not transaction:
+        return abort(404, {'model': 'Transaction', 'id': transaction_id})
+
+    message = f'Transaction {transaction_id} has been updated'
+    return {'success': True, 'message': message, 'data': transaction}, 200
+
+@bp.route('/accounts/<int:account_id>/transactions/<int:transaction_id>',
+          methods=['DELETE'])
+def delete_transaction(account_id, transaction_id):
     """Api router for a single account transaction resource"""
     account = Accounts.query.get(account_id)
     transaction = None
@@ -148,15 +215,5 @@ def get_transaction(account_id, transaction_id):
     if not transaction:
         return abort(404, {'model': 'Transaction', 'id': transaction_id})
 
-    if request.method == 'GET':
-        return {'success': True,
-                'message': 'Transaction found',
-                'data': transaction}, 200
-
-    elif request.method == 'PATCH':
-        message = f'Transaction {transaction_id} has been updated'
-        return {'success': True, 'message': message, 'data': transaction}, 200
-
-    else:
-        message = f'Transaction {transaction_id} has been deleted'
-        return {'success': True, 'message': message, 'data': {}}, 200
+    message = f'Transaction {transaction_id} has been deleted'
+    return {'success': True, 'message': message, 'data': {}}, 200
