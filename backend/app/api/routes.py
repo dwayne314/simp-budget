b919661d6654f08e5f2914a3e4d75d88cfb559f9
dwayne314 <dwayne.manlove@gmail.com>
2020-05-18 19:18:12 -0400

    Return the user's id when requesting an api token

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -26,7 +26,7 @@ def get_token():
     """Returns the auth token for a logged in user"""
     token = g.current_user.get_token()
     db.session.commit()
-    return jsonify({'token': token})
+    return jsonify({'token': token, 'id': g.current_user.id})
 
 @bp.route('/tokens', methods=['DELETE'])
 @token_auth.login_required
