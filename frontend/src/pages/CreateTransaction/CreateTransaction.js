5316af4967b08cac282ec2fe797b25d750463a8c
dwayne314 <dwayne.manlove@gmail.com>
2020-05-17 22:40:00 -0400

    Create NewTransaction and EditTransaction views

--- frontend/src/pages/CreateTransaction/CreateTransaction.js
+++ frontend/src/pages/CreateTransaction/CreateTransaction.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './CreateTransaction.css';
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
 
 
 const CreateTransaction = () => {
     return (
-        <div className="create-transaction-container">
-            CreateTransaction
+        <div className="new-transaction-container">
+
+            <div className="new-transaction-logo-container">
+                <Logo isPrimary={false}/>
+            </div>
+            <div className="new-transaction-form-container">
+                <div className="new-transaction-form-header">
+                    New Transaction
+                </div>
+                <div className="new-transaction-form">
+                    
+                    <form>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="amount">Amount</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="amount"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="note">Note</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="note"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container form-button-container">
+                            <Button cta={"Submit"} isPrimary={false}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     );
 };
--- frontend/src/pages/EditTransaction/EditTransaction.js
+++ frontend/src/pages/EditTransaction/EditTransaction.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './EditTransaction.css';
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
 
 
 const EditTransaction = () => {
     return (
         <div className="edit-transaction-container">
-            EditTransaction
+
+            <div className="edit-transaction-logo-container">
+                <Logo isPrimary={false}/>
+            </div>
+            <div className="edit-transaction-form-container">
+                <div className="edit-transaction-form-header">
+                    Edit Transaction
+                </div>
+                <div className="edit-transaction-form">
+                    
+                    <form>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="amount">Amount</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="amount"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="note">Note</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="note"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container form-button-container">
+                            <Button cta={"Submit"} isPrimary={false}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     );
 };
