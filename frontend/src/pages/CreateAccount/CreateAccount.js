e505421d7504b1d95c0c0aed712718f0f56078a3
dwayne314 <dwayne.manlove@gmail.com>
2020-05-17 23:34:53 -0400

    Implement Create Account and Edit Account froms

--- frontend/src/pages/CreateAccount/CreateAccount.js
+++ frontend/src/pages/CreateAccount/CreateAccount.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './CreateAccount.css';
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
 
 
 const CreateAccount = () => {
     return (
-        <div className="create-account-container">
-            CreateAccount
+        <div className="new-account-container">
+
+            <div className="new-account-logo-container">
+                <Logo isPrimary={false}/>
+            </div>
+            <div className="new-account-form-container">
+                <div className="new-account-form-header">
+                    New Account
+                </div>
+                <div className="new-account-form">
+                    
+                    <form>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="name"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="description">Description</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="description"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container form-button-container">
+                            <Button cta={"Submit"} isPrimary={false}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     );
 };
--- frontend/src/pages/EditAccount/EditAccount.js
+++ frontend/src/pages/EditAccount/EditAccount.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './EditAccount.css';
+import Logo from '../../components/Logo/Logo';
+import Button from '../../components/Button/Button';
 
 
 const EditAccount = () => {
     return (
         <div className="edit-account-container">
-            EditAccount
+
+            <div className="edit-account-logo-container">
+                <Logo isPrimary={false}/>
+            </div>
+            <div className="edit-account-form-container">
+                <div className="edit-account-form-header">
+                    Edit Account
+                </div>
+                <div className="edit-account-form">
+                    
+                    <form>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="name"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container">
+                            <div className="form-label">
+                                <label htmlFor="description">Description</label>
+                            </div>
+                            <div className="form-input">
+                                <input type="text" id="description"/>
+                            </div>
+                        </div>
+                        <div className="form-item-container form-button-container">
+                            <Button cta={"Submit"} isPrimary={false}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
         </div>
     );
 };
