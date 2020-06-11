83054508f264e57b4149aa2dd396b003a6a95291
dwayne314 <dwayne.manlove@gmail.com>
2020-06-11 13:05:33 -0400

    Add home section two
    The second section of the home page, showing what the app offers is now
    implemented to give users a better idea of what they are signing up
    for.

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import TrackSpendingIcon from '../../static/icons/track-spending.svg';
+import GenerateReportIcon from '../../static/icons/report.svg';
+import LimitSpendingIcon from '../../static/icons/limit-spending.svg';
 import './Home.css';
 
 
@@ -23,9 +26,7 @@ const Home = () => {
                         </div>
                         <div className="home-button">
                             <Button isPrimary={true} cta={"Start Saving"} linkPath="/login"/>
-
                         </div>
-
                     </div>
                     <div className="home-footer-container">
                         <div className="">
@@ -38,7 +39,39 @@ const Home = () => {
                     </div>                
                 </div>
             </div>
-            <div className="home-page-2-container">page 3</div>
+            <div href="page"className="home-page-2-container">
+                <div className="home-page-two">
+                    <div className="page-two-header-container">
+                        How It Works
+                    </div>
+                    <div className="how-it-works-container">
+                        <div className="how-it-works">
+                            <div className="how-it-works-icon">
+                                <img src={TrackSpendingIcon} alt="a picture of a receipt"/>
+                            </div>
+                            <div className="how-it-works-header">Enter Your Transactions</div>                            
+                            <div className="how-it-works-body">With our easy to use transaction logging interface, inserting new account transactions is effortless.</div>
+                        </div>
+                        <div className="how-it-works">
+                            <div className="how-it-works-icon" alt="scissors cutting a coupon">
+                                <img src={LimitSpendingIcon}/>
+                            </div>
+                            <div className="how-it-works-header">Set Spending Limits</div>                            
+                            <div className="how-it-works-body">Easily set up spending limits on user, account, or transaction levels that allows you to take full control of your spending.</div>
+                        </div>
+                        <div className="how-it-works">
+                            <div className="how-it-works-icon">
+                                <img src={GenerateReportIcon} alt="a picture of a report"/>
+                            </div>
+                            <div className="how-it-works-header">Generate Reports</div>                            
+                            <div className="how-it-works-body">Budget reporting has never been easier with custom account health exports sent to you at daily, weekly, or monthly intervals.</div>
+                        </div>
+                    </div>
+                    <div className="home-button">
+                        <Button isPrimary={true} cta={"Start Saving"} linkPath="/login"/>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     );
 };
